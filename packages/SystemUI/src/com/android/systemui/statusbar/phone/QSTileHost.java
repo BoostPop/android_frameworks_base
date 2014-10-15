c0e3df3a590627bd4aaa990a89b4f6f0560fa9a3
kaiyiz <kaiyiz@codeaurora.org>
2014-10-15 10:20:02 +0800

    SystemUI: Add sound item in quick settings panel
    Add sound item in quick settings panel.
    
    CRs-Fixed: 715258
    
    [Crostantin]: Adapted for BoostPop
    
    Change-Id: I090929a3622101edc5413afe4f7c2d1863139d2c
    
    Signed-off-by: Crostantin <crostantin@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java
@@ -41,6 +41,7 @@ import com.android.systemui.qs.tiles.HotspotTile;
 import com.android.systemui.qs.tiles.IntentTile;
 import com.android.systemui.qs.tiles.LocationTile;
 import com.android.systemui.qs.tiles.NfcTile;
+import com.android.systemui.qs.tiles.NotificationsTile;
 import com.android.systemui.qs.tiles.RotationLockTile;
 import com.android.systemui.qs.tiles.ScreenOffTile;
 import com.android.systemui.qs.tiles.SyncTile;
@@ -59,6 +60,7 @@ import com.android.systemui.statusbar.policy.HotspotController;
 import com.android.systemui.statusbar.policy.SecurityController;
 import com.android.systemui.statusbar.policy.UserSwitcherController;
 import com.android.systemui.statusbar.policy.ZenModeController;
+import com.android.systemui.volume.VolumeComponent;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -85,6 +87,7 @@ public class QSTileHost implements QSTile.Host {
     private final CastController mCast;
     private final Looper mLooper;
     private final CurrentUserTracker mUserTracker;
+    private final VolumeComponent mVolume;
     private final FlashlightController mFlashlight;
     private final UserSwitcherController mUserSwitcherController;
     private final KeyguardMonitor mKeyguard;
@@ -95,7 +98,7 @@ public class QSTileHost implements QSTile.Host {
     public QSTileHost(Context context, PhoneStatusBar statusBar,
             BluetoothController bluetooth, LocationController location,
             RotationLockController rotation, NetworkController network,
-            ZenModeController zen, HotspotController hotspot,
+            ZenModeController zen, VolumeComponent volume, HotspotController hotspot,
             CastController cast, FlashlightController flashlight,
             UserSwitcherController userSwitcher, KeyguardMonitor keyguard,
             SecurityController security) {
@@ -106,6 +109,7 @@ public class QSTileHost implements QSTile.Host {
         mRotation = rotation;
         mNetwork = network;
         mZen = zen;
+        mVolume = volume;
         mHotspot = hotspot;
         mCast = cast;
         mFlashlight = flashlight;
@@ -197,6 +201,11 @@ public class QSTileHost implements QSTile.Host {
     }
 
     @Override
+    public VolumeComponent getVolumeComponent() {
+        return mVolume;
+    }
+
+    @Override
     public HotspotController getHotspotController() {
         return mHotspot;
     }
@@ -259,6 +268,7 @@ public class QSTileHost implements QSTile.Host {
         else if (tileSpec.equals("ScreenshotTile")) return new ScreenshotTile(this);
         else if (tileSpec.equals("adb_network")) return new AdbOverNetworkTile(this);
         else if (tileSpec.equals("usb_tether")) return new UsbTetherTile(this);
+        else if (tileSpec.equals("notifications")) return new NotificationsTile(this);
         else if (tileSpec.startsWith(IntentTile.PREFIX)) return IntentTile.create(this,tileSpec);
         else throw new IllegalArgumentException("Bad tile spec: " + tileSpec);
     }
