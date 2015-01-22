1dfe9422eff5f9c041848a799e8d3b154ac65018
Adnan Begovic <adnan@cyngn.com>
2015-01-21 18:54:39 -0800

    SystemUI: Create LteTile
    Change-Id: Ic193279f930af54d5754358c7e4c97b6b07d2010
    Signed-off-by: Crostantin <crostantin@gmail.com>
    
    Conflicts:
    	core/java/com/android/internal/util/cm/QSConstants.java
    	core/java/com/android/internal/util/cm/QSUtils.java
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java
@@ -42,6 +42,7 @@ import com.android.systemui.qs.tiles.ImmersiveTile;
 import com.android.systemui.qs.tiles.IntentTile;
 import com.android.systemui.qs.tiles.LocationTile;
 import com.android.systemui.qs.tiles.NfcTile;
+import com.android.systemui.qs.tiles.LteTile;
 import com.android.systemui.qs.tiles.NotificationsTile;
 import com.android.systemui.qs.tiles.RotationLockTile;
 import com.android.systemui.qs.tiles.ScreenOffTile;
@@ -271,6 +272,7 @@ public class QSTileHost implements QSTile.Host {
         else if (tileSpec.equals("usb_tether")) return new UsbTetherTile(this);
         else if (tileSpec.equals("notifications")) return new NotificationsTile(this);
         else if (tileSpec.equals("immersive")) return new ImmersiveTile(this);
+        else if (tileSpec.equals("lte")) return new LteTile(this);
         else if (tileSpec.startsWith(IntentTile.PREFIX)) return IntentTile.create(this,tileSpec);
         else throw new IllegalArgumentException("Bad tile spec: " + tileSpec);
     }
