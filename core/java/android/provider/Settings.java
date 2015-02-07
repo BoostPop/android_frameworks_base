bf5360dc08d4a9353a2c72f1ccd2dedb31edfa34
maxwen <max.weninger@gmail.com>
2015-02-07 03:00:51 +0100

    [1/2] base: storage notification enhancement
    -allow hiding storage removed notification
    this notification can be pretty anyoing e.g. for
    usb otg storage devices
    
    -ongoing mount notifications for removable
    storage devices
    
    -to prevent unnice resizing when using progress in
    the notification change the notification layout height
    of the progress bar to be the same as the text line
    
    Change-Id: If6401cca67155fe14d4db01eadac81ca3b55e8cf
    
    Conflicts:
    	core/java/android/provider/Settings.java
    	packages/SystemUI/res/values/custom_strings.xml

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2679,6 +2679,20 @@ public final class Settings {
         public static final String VOLUME_ADJUST_SOUND = "volume_adjust_sound";
 
         /**
+         * Should the storage removed notification be shown
+         *
+         * @hide
+         */
+        public static final String STORAGE_MEDIA_REMOVED_NOTIFICTION = "storage_media_removed_notification";
+
+        /**
+         * Enable ongoing mount notifications for those storage volumes
+         *
+         * @hide
+         */
+        public static final String STORAGE_MOUNT_NOTIFICATION = "storage_mount_notification";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
