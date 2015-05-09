8f52f92c6302e68f0f16517d4a603bdf2bb05961
Crostantin <crostantin@gmail.com>
2015-05-09 17:04:19 +0200

    QS: Nfc dynamic tile
    Using the logic of stock tether tile make CM nfc tile dynamic
    tile disappears after 14 days if not used. Long press hides tile
    
    thanks to @CyanogenMod for the drawables and basic code.
    
    Change-Id: Ifd06e037a9aa1f373338756bc575ae3508282d34
    Signed-off-by: Crostantin <crostantin@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java
@@ -40,6 +40,7 @@ import com.android.systemui.qs.tiles.FlashlightTile;
 import com.android.systemui.qs.tiles.HotspotTile;
 import com.android.systemui.qs.tiles.IntentTile;
 import com.android.systemui.qs.tiles.LocationTile;
+import com.android.systemui.qs.tiles.NfcTile;
 import com.android.systemui.qs.tiles.RotationLockTile;
 import com.android.systemui.qs.tiles.UsbTetherTile;
 import com.android.systemui.qs.tiles.WifiTile;
@@ -265,6 +266,7 @@ public class QSTileHost implements QSTile.Host {
         else if (tileSpec.equals("hotspot")) return new HotspotTile(this);
         else if (tileSpec.equals("usb_tether")) return new UsbTetherTile(this);
         else if (tileSpec.equals("adb_network")) return new AdbOverNetworkTile(this);
+        else if (tileSpec.equals("nfc")) return new NfcTile(this);
         else if (tileSpec.startsWith(IntentTile.PREFIX)) return IntentTile.create(this,tileSpec);
         else throw new IllegalArgumentException("Bad tile spec: " + tileSpec);
     }
